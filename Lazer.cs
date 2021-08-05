182cb550543c37ff76a113044d702fcab35bf5d7
platypus567 <63357002+platypus567@users.noreply.github.com>
2021-08-05 14:42:53 -0400

    Add files via upload

---
+++ Lazer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 8;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laserMovement();
+        //if laser leaves screen, delete.
+        if(transform.position.y > 5.7f){
+            Destroy(gameObject);
+        }
+    }
+    void laserMovement()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 3.5f;
+    public float horizontalInput;
+    public float verticalInput;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    void Start()
+    {
+        //take current position and assign new
+
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+        //if i hit space, spawn gameobject.
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+    }
+    void PlayerMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -2.7f, 0), 0);
+        if (transform.position.x > 9.635843)
+        {
+            transform.position = new Vector3(-9, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.635843)
+        {
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+    }
+}
