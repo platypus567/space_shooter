fc0717d267b2bfb06e700dfc206309e6ae2d83a9
platypus567 <63357002+platypus567@users.noreply.github.com>
2021-08-05 16:24:35 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyMovement();
+        
+
+
+    }
+    void enemyMovement()
+    {
+                //move enemy down at 4 meters per second
+        //if hit bottom of screen, respawn at top
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if(transform.position.y <= -5f)
+        {
+            transform.position = new Vector3(Random.Range(-7.11f, 7.11f), 5.7f, 0);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    { 
+        if(other.tag == "Player"){
+        Destroy(this.gameObject);
+        }
+        if(other.tag == "Lazer"){
+            Destroy(other);
+            Destroy(this.gameObject);
+        }
+    }
+  
+}
